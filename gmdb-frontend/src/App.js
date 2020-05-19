8c7c549891e8e81a9be1e505d7f125f42333a19b
GORTONJ <jgorton78@gmail.com>
2020-05-19 10:39:08 -0600

    updated files

--- gmdb-frontend/src/App.js
+++ gmdb-frontend/src/App.js
@@ -5,38 +5,41 @@ class App extends React.Component{
 
   state = {  
     movies: [],  
-    error: "" 
-  }
-  
+  } 
+
+
   async componentDidMount() {
       let response = await fetch('http://penguin.linux.test:3001/movies')
-      let data = await response.json()
-        console.log(data)
-        this.setState( {movies: data} )
-    }  
+      let data = await response.json()      
+        console.log(data[0])
+      let tempArray = []  
 
+        data.forEach(movie => {
+          tempArray.push(movie.title)
+        })
+
+        this.setState( {movies: tempArray} )
+  }    
+  
   render(){
-
+    const {movies} = this.state
     return (    
 
         <div className="App">
           <header className="App-header">
           <h2>GMDB</h2>
           <ul id="navigation">        
-            <li><a href="news.asp">Home</a></li>
-            <li><a href="contact.asp">Login</a></li>
+            <li>Home</li>
+            <li>Login</li>
             <li>
               <input type="text" placeholder="Search"/>
               <button type="submit">Search</button>
             </li>
           </ul>       
-          </header>          
-          
+          </header> 
           <div>
-            {this.state.movies[0]}
-            {this.state.error}
+            {this.state.movies}
           </div>
-
         </div>
       );
   }
